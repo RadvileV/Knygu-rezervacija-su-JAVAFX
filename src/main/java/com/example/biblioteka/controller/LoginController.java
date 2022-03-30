e61fc17bb416ef535c01eb48a371f302fd68966e
RadvileV <radvilevinciauskaite@gmail.com>
2022-03-30 14:23:42 +0300

    Istrintos neveikiancios funkcijos, galutinis variantas

--- src/main/java/com/example/biblioteka/controller/LoginController.java
+++ src/main/java/com/example/biblioteka/controller/LoginController.java
@@ -83,7 +83,7 @@ public class LoginController {
         Parent root = FXMLLoader.load(MainApplication.class.getResource("userDashboard-view.fxml"));
         Stage registerStage = new Stage();
         registerStage.setTitle("Vartotojo langas");
-        registerStage.setScene(new Scene(root, 1100, 700));
+        registerStage.setScene(new Scene(root, 1100, 400));
         registerStage.show();
         ((Node) (event.getSource())).getScene().getWindow().hide();
     }
